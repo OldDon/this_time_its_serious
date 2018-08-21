0a061057caf78b97d2ae1ffc906e6580729ac762
Dave Elliott <davey.elliott@gmail.com>
2018-08-21 23:13:38 +0100

    split code-imports at start of script-backbone in function

--- chap-04_random_number_game.py
+++ chap-04_random_number_game.py
@@ -8,13 +8,17 @@ The computer only responds with three answers:
 """
 
 import random
-n = random.randint(1, 100)
-while True:
-    ans = int(input('Enter your guess: '))
-    if ans == n:
-        print('Success! You win!')
-        break
-    elif ans > n:
-        print('Too high!')
-    else:
-        print('Too low')
+
+def newmethod57():
+    n = random.randint(1, 100)
+    while True:
+        ans = int(input('Enter your guess: '))
+        if ans == n:
+            print('Success! You win!')
+            break
+        elif ans > n:
+            print('Too high!')
+        else:
+            print('Too low')
+
+newmethod57()
