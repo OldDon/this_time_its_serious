0850a6f32ec682d883cd47994fa724754826a46b
Dave Elliott <davey.elliott@gmail.com>
2018-08-22 03:49:06 +0100

    Ex4.3.2_Add explanatory text to the start of the game.

--- chap-04_random_number_game.py
+++ chap-04_random_number_game.py
@@ -7,7 +7,16 @@ The computer only responds with three answers:
 3. Correct
 """
 
+
 import random
+
+print('Hi there. This is a little program to pass some time in an "amusing" way (you can be the judge of that!)')
+print('\n')
+print('The aim of the game is this:')
+print('The computer will "guess" a random number between 1 - 100')
+print('Your job, should you wish to accept it Mr. Phelps, is to guess that number in the fewest number of guesses!')
+print('.....don\'t worry though, the computer will give you clues as to the direction you need to be going to figure it out')
+print('Enjoy :)')
 
 
 n = random.randint(1, 100)
