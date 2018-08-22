3b2c0fab25b09b951fd797c3d8f8fab366d79972
Dave Elliott <davey.elliott@gmail.com>
2018-08-22 01:53:06 +0100

    Removed play/no play and also function wrapper

--- chap-04_random_number_game.py
+++ chap-04_random_number_game.py
@@ -22,15 +22,8 @@ def random_game():
         else:
             print('Too low')
 
-random_game()
 
-def play():
-    play_again = input('Do you want to play again? ((y)es or (n)o ?')
-    if play_again == 'n':
-        print('OK. Hope you had a good time. Thank and bye bye :)')
-        sys.exit
-    else:
-        random_game()
+
 
         
     
